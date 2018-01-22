688bdcf2106156c1634d4dcae729cc0b28791eb7
jschouwstra <schouwstra.jelle@gmail.com>
2018-01-22 12:58:51 +0100

    Added files

---
+++ web/sites/default/files/php/twig/5a59e487b5baf_views-view-list.html.twig_i4TukcQ2SS_a_p-IBTHp2a3dV/uv8zgQo6wPhN6XBwsCrUQ4dRJhouYUWpwu1wLbTAzxI.php
@@ -0,0 +1,132 @@
+<?php
+
+/* themes/drupal8_zymphonies_theme/templates/views/views-view-list.html.twig */
+class __TwigTemplate_854f4b17bbd87eeb2d7cfee7508b15f7e0070e7953cd80685469887153925a13 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 22, "for" => 31);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if', 'for'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 21
+        echo "
+";
+        // line 22
+        if ((isset($context["attributes"]) ? $context["attributes"] : null)) {
+            // line 23
+            echo "<div";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+            echo ">
+";
+        }
+        // line 25
+        echo "  ";
+        if ((isset($context["title"]) ? $context["title"] : null)) {
+            // line 26
+            echo "    <h3>";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["title"]) ? $context["title"] : null), "html", null, true));
+            echo "</h3>
+  ";
+        }
+        // line 28
+        echo "
+  <";
+        // line 29
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["list"]) ? $context["list"] : null), "type", array()), "html", null, true));
+        echo " ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["list"]) ? $context["list"] : null), "attributes", array()), "html", null, true));
+        echo " ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(((($this->getAttribute((isset($context["list"]) ? $context["list"] : null), "type", array()) == "ul")) ? ("class=\"arrow_list\"") : (""))));
+        echo " >
+
+    ";
+        // line 31
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["rows"]) ? $context["rows"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["row"]) {
+            // line 32
+            echo "      <li";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute($context["row"], "attributes", array()), "html", null, true));
+            echo ">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute($context["row"], "content", array()), "html", null, true));
+            echo "</li>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['row'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 34
+        echo "
+  </";
+        // line 35
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["list"]) ? $context["list"] : null), "type", array()), "html", null, true));
+        echo ">
+
+";
+        // line 37
+        if ((isset($context["attributes"]) ? $context["attributes"] : null)) {
+            // line 38
+            echo "</div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/drupal8_zymphonies_theme/templates/views/views-view-list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  100 => 38,  98 => 37,  93 => 35,  90 => 34,  79 => 32,  75 => 31,  66 => 29,  63 => 28,  57 => 26,  54 => 25,  48 => 23,  46 => 22,  43 => 21,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/drupal8_zymphonies_theme/templates/views/views-view-list.html.twig", "C:\\wamp64\\www\\js-wiki\\web\\themes\\drupal8_zymphonies_theme\\templates\\views\\views-view-list.html.twig");
+    }
+}
